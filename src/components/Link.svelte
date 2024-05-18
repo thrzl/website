0f27fde5a8dd36886436c7519632e6cf6c1c9570
te <thrzl@icloud.com>
2024-05-18 15:16:50 -0400

    🎨 remove unnecessary type

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 export let href: string;
 //biome-ignore lint/style/useConst: can't be const as it is defined in the svelte component
-export let _class: string = "";
+export let _class = "";
 </script>
 
 <a {href} class={`group ${_class}`}
