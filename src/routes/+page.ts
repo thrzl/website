7722639b50d22772738f6775cd995578b3344bb2
te <thrzl@icloud.com>
2024-07-03 18:36:55 -0400

    ⚡️ cache page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,11 @@
 /** @type {import('./$types').PageServerLoad} */
-export function load({ fetch }) {
+export function load({ fetch, setHeaders }) {
 	function get(endpoint: string) {
 		return fetch(`/api/${endpoint}`).then((res) => res.json());
 	}
+
+	setHeaders({ "Cache-Control": "max-age=3600" });
+
 	return {
 		music: get("music"),
 		projects: get("projects"),
