1750cb701717aafc969cbc9524f8f9483b4c83ed
te <thrzl@icloud.com>
2025-03-11 08:27:18 -0400

    change time period to last week to ensure data is always there

--- src/routes/api/music/+server.ts
+++ src/routes/api/music/+server.ts
@@ -7,7 +7,7 @@ export async function GET({ url, setHeaders }) {
   if (username === "favicon.ico") return json({ status: 404 });
 
   const res = await fetch(
-    `https://api.listenbrainz.org/1/stats/user/${username}/releases?range=this_week&count=20`,
+    `https://api.listenbrainz.org/1/stats/user/${username}/releases?range=week&count=40`,
   );
 
   setHeaders({ "Cache-Control": "max-age=3600" });
